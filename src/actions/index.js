5e1c121bcfa7278b4813dc594b1ac4f470754456
alpenfritz <mymailismissing@gmail.com>
2018-04-04 13:57:05 +0900

    Create action for selectBook action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,8 @@
 // action creators
 export function selectBook(book) {
-  console.log('A book has been selected:', book.title);
+  // returns an action (= object with a TYPE property)
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book,
+  };
 }
