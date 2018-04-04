5810e3215bdd3896df4fdb7f187016af5c4277e6
alpenfritz <mymailismissing@gmail.com>
2018-04-04 15:24:45 +0900

    Add BookDetail component

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import BookList from '../containers/bookList';
+import BookDetail from '../containers/bookDetail';
 
 class App extends Component {
   render() {
     return (
       <div>
         <BookList />
+        <BookDetail />
       </div>
     );
   }
