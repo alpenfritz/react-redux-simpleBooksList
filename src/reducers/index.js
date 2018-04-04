9004373581140ea181308a2799c356c6d4443b6d
alpenfritz <mymailismissing@gmail.com>
2018-04-04 14:14:46 +0900

    Create activeBook reducer (consume action in reducer)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import BookReducer from './reducerBooks';
+import ActiveBook from './reducerActiveBook';
 
 const rootReducer = combineReducers({
   books: BookReducer,
+  activeBook: ActiveBook,
 });
 
 export default rootReducer;
