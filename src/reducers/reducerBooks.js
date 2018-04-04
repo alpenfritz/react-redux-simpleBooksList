980a1b3fc7f41a1700f1294a6489516bb0dff19f
alpenfritz <mymailismissing@gmail.com>
2018-04-04 15:42:40 +0900

    Add more details to books (activeBook.pages)

--- src/reducers/reducerBooks.js
+++ src/reducers/reducerBooks.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    {title: 'JavaScript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'Lord of the Rings'},
-    {title: 'Eloquent Ruby'},
+    {title: 'JavaScript: The Good Parts', pages: 101},
+    {title: 'Harry Potter', pages: 39},
+    {title: 'Lord of the Rings', pages: 85},
+    {title: 'Eloquent Ruby', pages: 10},
   ]
 }
