d90bd3a2f0d013a55a128766088996d6d8ba7a94
alpenfritz <mymailismissing@gmail.com>
2018-04-04 15:38:11 +0900

    Link React and Redux for BookDetail

--- src/containers/bookDetail.js
+++ src/containers/bookDetail.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if(!this.props.activeBook) {
+      return <div>Select a book to get started.</div>
+    }
+
     return (
-      <div>Book Detail!</div>
+      <div>
+        <h3>Details for:</h3>
+        <div>{this.props.activeBook.title}</div>
+      </div>
     );
   }
 }
 
-export default BookDetail;
+function mapStateToProps(state){
+  return {
+    activeBook: state.activeBook,
+  }
+}
+
+export default connect(mapStateToProps)(BookDetail);
